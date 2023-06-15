23d86f291a011b47b46162d26b124bcfc1d1bb17
Lorenzo Pilla <62301813+lpilla@users.noreply.github.com>
2023-06-15 23:28:25 +0200

    fix: fixed environment variabel for vite projects

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,15 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: "olimpia-pet.appspot.com",
   messagingSenderId: "1038746979749",
   appId: "1:1038746979749:web:691eb8fcda1f55e6be6c4c",
@@ -17,3 +17,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
