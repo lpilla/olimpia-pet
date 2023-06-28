52558f3eb5e642d71560b983038669ac7fcef926
Lorenzo Pilla <infopillingoita@gmail.com>
2023-06-28 11:12:23 +0200

    feat: added custom card on marker click

--- src/components/CustomMarker.jsx
+++ src/components/CustomMarker.jsx
@@ -3,11 +3,14 @@ import {
   DialogHeader,
   DialogBody,
   DialogFooter,
-  Button,
+  Button, Typography, CardHeader, CardBody, Chip,
 } from "@material-tailwind/react";
 import L from "leaflet";
 import { useState } from "react";
 import { Marker } from "react-leaflet";
+import Card from "@material-tailwind/react/components/Card";
+import { ArrowLongRightIcon } from "@heroicons/react/24/outline";
+import { PhoneIcon } from "@heroicons/react/24/outline";
 
 const CustomMarker = ({ marker }) => {
   const [dialogOpen, setDialogOpen] = useState(false);
@@ -20,32 +23,56 @@ const CustomMarker = ({ marker }) => {
     iconSize: [60, 60], // Adjust the size according to your icon
   });
   return (
-    <Marker
-      position={marker.coordinates}
-      icon={icon}
-      eventHandlers={{
-        click: () => {
-          handleDialogOpen();
-        },
-      }}
-    >
-      <Dialog
-        open={dialogOpen}
-        handler={handleDialogOpen}
-        animate={{
-          mount: { scale: 1, y: 0 },
-          unmount: { scale: 0.9, y: -100 },
-        }}
+      <Marker
+          position={marker.coordinates}
+          icon={icon}
+          eventHandlers={{
+            click: () => {
+              handleDialogOpen();
+            },
+          }}
       >
-        <DialogHeader>{marker.name}</DialogHeader>
-        <DialogBody divider>{marker.description}</DialogBody>
-        <DialogFooter>
-          <Button variant="gradient" color="green" onClick={handleDialogOpen}>
-            <span>Confirm</span>
-          </Button>
-        </DialogFooter>
-      </Dialog>
-      {/* <Popup
+        <Dialog
+            open={dialogOpen}
+            handler={handleDialogOpen}
+            animate={{
+              mount: {scale: 1, y: 0},
+              unmount: {scale: 0.9, y: -100},
+            }}
+            className={"p-0 m-0"}
+        >
+          <DialogHeader>{marker.name}</DialogHeader>
+          <DialogBody className={"p-0"}> <Card className="flex-row w-full max-w-[48rem] p-0 rounded-none shadow-none">
+            <CardHeader shadow={false} floated={false} className="w-2/5 shrink-0 m-0 rounded-none">
+              <img
+                  src={marker.icon}
+                  alt="image"
+                  className="w-full h-full object-cover"
+              />
+            </CardHeader>
+            <CardBody>
+              <div className="flex flex-wrap gap-1">
+                {marker.categories.map((cat) => {
+                  return  <Chip variant="ghost" value={cat} />
+                })}
+              </div>
+              <Typography variant="h4" color="blue-gray" className="my-2">
+                Lyft launching cross-platform service this week
+              </Typography>
+              <Typography color="gray" className="font-normal mb-8">
+                {marker.description}
+              </Typography>
+              <a href="#" className="inline-block">
+                <Button variant="text" className="flex items-center gap-2" color={"green"}>
+                  <PhoneIcon strokeWidth={2} className="w-4 h-4"/>
+                  Contattaci Subito
+                </Button>
+              </a>
+            </CardBody>
+          </Card>
+          </DialogBody>
+        </Dialog>
+        {/* <Popup
             minWidth={1000}
             closeOnEscapeKey
             closeButton={false}
@@ -76,7 +103,7 @@ const CustomMarker = ({ marker }) => {
               </Card>
             
           </Popup> */}
-    </Marker>
+      </Marker>
   );
 };
 export default CustomMarker;
