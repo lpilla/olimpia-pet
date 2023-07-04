01627ca02c08609b4a4ba8b34f186d13d1ec88fa
Tommaso Galimberti <galimba615@gmail.com>
2023-07-04 23:11:12 +0200

    feat: added a dogs' photos
    Aggiunte le foto dei cani per ciascuna carta

--- src/components/animalCard.jsx
+++ src/components/animalCard.jsx
@@ -5,17 +5,35 @@ import {
   Typography,
   Button,
 } from "@material-tailwind/react";
+import { useState, useEffect } from "react";
 
-const AnimalCard = ({ titolo }) => {
+const AnimalCard = ({ breed }) => {
+  const [foto, setFoto] = useState("");
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breed/" + breed + "/images/random")
+      .then((response) => response.json())
+      .then((json) => {
+        setFoto(json);
+      });
+  }, []);
+
   return (
     <>
       <Card className="w-56 border-t-2">
-        <CardHeader floated={false} className="h-70">
-          <img src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="profile-picture" />
+        <CardHeader
+          floated={false}
+          className="h-70 flex items-center justify-center"
+        >
+          <img
+            src={foto.message}
+            alt="profile-picture"
+            className="max-w-[300px] max-h-[150px]"
+          />
         </CardHeader>
         <CardBody className="text-center">
           <Typography variant="h5" color="blue-gray" className="mb-2">
-            {titolo}
+            {breed}
           </Typography>
         </CardBody>
       </Card>
