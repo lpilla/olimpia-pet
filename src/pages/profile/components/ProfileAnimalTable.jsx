4dd6964c756e1030adca7486f4586d70722073f3
Tommaso Galimberti <galimba615@gmail.com>
2023-07-06 01:05:46 +0200

    fix: fixed AnimalTable
    Utilizzato grid per la lista degli animali

--- src/pages/profile/components/ProfileAnimalTable.jsx
+++ src/pages/profile/components/ProfileAnimalTable.jsx
@@ -4,45 +4,37 @@ import { UserContext } from "../../../context/UserContext";
 import { Button, Typography, Avatar } from "@material-tailwind/react";
 import FotoCliccabile from "./FotoCliccabile";
 import { useState } from "react";
+import { useEffect } from "react";
 
 const ProfileAnimalTable = () => {
   const { userObj } = useContext(UserContext);
   const [idCancellazione, setId] = useState("");
+  const [lista, setLista] = useState(userObj.lista);
+
+  useEffect(() => {
+    setLista(userObj.lista);
+  }, [userObj]);
+
   const catchId = (v) => {
     setId(v);
+    //Id per cancellare Animale dalla lista
   };
 
-  return (
-    <>
-      <div className="flex items-start justify-start m-10">
-        {userObj.lista.map((dog) => {
-          return (
-            <div className="flex flex-col items-center justify-center">
-              <h5 className="text-2xl font-bold text-blue-900">{dog.nome}</h5>
-              <FotoCliccabile dog={dog} sendId={catchId} />
-            </div>
-          );
-        })}
-      </div>
-
-      {/*
-      lista.map((dog) => (
-        <div className="grid grid-cols-2 gap-[20rem] grid-rows-1 w-[full-4rem] h-[5rem] border-2 border-r-2 rounded-lg border-blue-300 ml-[10rem] mr-[10rem]">
-          <div className="flex justify-center items-center "></div>
-          <div className="flex space-x-4 justify-center items-center">
-            <Typography variant="h4" className="flex pr-4">
-              Nome:{dog.nome}
-            </Typography>
-            <Button variant="text">Edit</Button>
-            <Button variant="text" className="text-red-500">
-              Remove
-            </Button>
-          </div>
+  if (lista)
+    return (
+      <>
+        <div className="grid grid-cols-3">
+          {lista.map((dog) => {
+            return (
+              <div className="flex flex-col items-center justify-center m-10">
+                <h5 className="text-2xl font-bold text-blue-900">{dog.nome}</h5>
+                <FotoCliccabile dog={dog} sendId={catchId} />
+              </div>
+            );
+          })}
         </div>
-      ))
-      */}
-    </>
-  );
+      </>
+    );
 };
 
 export default ProfileAnimalTable;
